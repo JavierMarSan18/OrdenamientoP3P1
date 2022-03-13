b6cc995570bde266650f4e4d979a8064496d648a
JavierMarSan18 <marroquinjavier75@gmail.com>
2022-03-13 12:47:36 -0600

    Final version

--- Colecciones/src/main/java/gt/edu/umg/progra3/parcial1/StaticStructureManager.java
+++ Colecciones/src/main/java/gt/edu/umg/progra3/parcial1/StaticStructureManager.java
@@ -1,9 +1,7 @@
 package gt.edu.umg.progra3.parcial1;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 public class StaticStructureManager<T> {
 
